bb79046a8c34d23033f7255b469fad90d7ebc042
Kenny mac <kennychavez.mtl@gmail.com>
2022-09-27 14:06:15 -0400

    completed grading students problem on hacker rank

--- grading_students.py
+++ grading_students.py
@@ -1,4 +1,4 @@
-# this is the solution to problem
+ # this is the solution to problem
 # https://www.hackerrank.com/challenges/three-month-preparation-kit-grading/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
 
 #!/bin/python3
