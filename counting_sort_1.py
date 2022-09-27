7263d58767ef3a0c78ca21d8ed830b6d7d24b428
Kenny mac <kennychavez.mtl@gmail.com>
2022-09-27 14:05:30 -0400

    completed counting_sort_1 problem on hacker rank

---
+++ counting_sort_1.py
@@ -0,0 +1,50 @@
+# this is my solution to problem
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr, n):
+    # Write your code here
+    
+    # we need to create an array filled with zeros with the lenght
+    # being equal to the highest number in the array (the lenght of this list is always 100)
+    result = [0] * 100
+
+    # we loop through the integers in the array
+    for integer in arr:
+        # we add one to the index of the number inside of the array filled
+        # with zeros
+        result[integer] += 1
+    
+    return result
+
+#test_case_1 = [63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32]
+# test_case_1 = [1,1,3,2,1]
+# print(countingSort(test_case_1))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
